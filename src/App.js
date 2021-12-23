61af43b1652e9388cd0f196fd4ac5922b3b0d493
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2021-12-23 12:57:13 -0500

    Initial Commit + Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <div className='container-fluid'>
+              <a className='navbar-brand' href='#'>
+                  CloudFlare Social
+              </a>
+          </div>
+      </nav>
     </div>
   );
 }
