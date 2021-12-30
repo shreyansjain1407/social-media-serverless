afa905ee86e49ef264b26a7df77eb42da09d5061
Shreyans Jain <42056701+shreyansjain1407@users.noreply.github.com>
2021-12-30 09:35:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
 
     const deployPost = async (uploadPost) => {
         const url = "https://social-media-worker.shreyansjain.workers.dev"
-        // const url = "http://127.0.0.1:8787"
         const id = Math.floor(Math.random() * 10000) + 1
         const newPost = {id, ...uploadPost}
         setPosts([newPost, ...posts])
